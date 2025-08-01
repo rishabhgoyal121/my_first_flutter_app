1f3214a25a8e6804061adf5711fbc960e4609f20
Rishabh Goyal <rishabh.goyal@uniqgridcloud.com>
2025-08-01 15:28:02 +0530

    feat: Implement CartScreen with cart item fetching and loading state management

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -8,11 +10,49 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  List<dynamic> cartItems = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCartItems();
+  }
+
+  Future<void> fetchCartItems() async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/carts/1'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        cartItems = data['products'];
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Cart')),
-      body: Center(child: Text('Your cart is empty')),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : cartItems.isEmpty
+          ? Center(child: Text('Your cart is empty'))
+          : ListView.builder(
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                final item = cartItems[index];
+                return ListTile(
+                  leading: Image.network(item['thumbnail'], width: 50),
+                  title: Text(item['title']),
+                  subtitle: Text('Quantity: ${item['quantity']}'),
+                );
+              },
+            ),
     );
   }
 }
