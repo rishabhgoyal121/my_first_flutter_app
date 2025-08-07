045cad1b6f3a9e94a5dec74bb775841bcac614ac
Rishabh Goyal <rishabh.goyal@uniqgridcloud.com>
2025-08-07 12:57:23 +0530

    feat: Enhance cart icon display; add cart count badge to ProductDetailsScreen

--- lib/product_details_screen.dart
+++ lib/product_details_screen.dart
@@ -19,16 +19,41 @@ class ProductDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int cartCount = context.watch<CartProvider>().cart['totalQuantity'];
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
         actions: [
-          IconButton(
-            key: cartIconKey,
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              Navigator.pushNamed(context, '/cart');
-            },
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              IconButton(
+                key: cartIconKey,
+                onPressed: () {
+                  Navigator.pushNamed(context, '/cart');
+                },
+                icon: Icon(Icons.shopping_cart),
+              ),
+              Positioned(
+                right: 8,
+                top: 4,
+                child: Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.yellowAccent,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Text(
+                    '$cartCount',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
