ac8dd4f3f737771246d1861fd1f64b8d5ba58df5
Rishabh Goyal <rishabhgoyal121@gmail.com>
2025-08-27 13:40:51 +0530

    Add haptic feedback and horizontal product list in Orders screen

--- lib/orders_screen.dart
+++ lib/orders_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_app/product.dart';
+import 'package:my_first_flutter_app/product_details_screen.dart';
 import 'package:provider/provider.dart';
 import 'order_provider.dart';
 import 'order_details_screen.dart';
@@ -36,10 +38,84 @@ class OrdersScreen extends StatelessWidget {
                         ),
                         SizedBox(height: 8),
                         Text('Products:'),
-                        ...products.map(
-                          (p) => Text(
-                            '${p['title']} x${p['quantity']} (\$${p['price']})',
-                            style: TextStyle(fontSize: 13),
+                        SizedBox(height: 8),
+                        SizedBox(
+                          height: 100,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: products.length,
+                            itemBuilder: (context, idx) {
+                              final p = products[idx];
+                              return InkWell(
+                                onTap: () {
+                                  HapticFeedback.lightImpact();
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (_) => ProductDetailsScreen(
+                                        product: Product.fromJson(p),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: Card(
+                                  margin: EdgeInsets.symmetric(horizontal: 4),
+                                  child: SizedBox(
+                                    width: 80,
+                                    child: Stack(
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Expanded(
+                                              child: Image.network(
+                                                p['thumbnail'] ?? '',
+                                                fit: BoxFit.cover,
+                                                errorBuilder:
+                                                    (
+                                                      context,
+                                                      error,
+                                                      stackTrace,
+                                                    ) => Icon(
+                                                      Icons.image_not_supported,
+                                                    ),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.all(4),
+                                              child: Text(
+                                                p['title'],
+                                                style: TextStyle(fontSize: 10),
+                                                textAlign: TextAlign.center,
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Positioned(
+                                          top: 0,
+                                          right: 0,
+                                          child: Container(
+                                            padding: EdgeInsets.all(2),
+                                            color: Colors.black.withValues(
+                                              alpha: 0.7,
+                                            ),
+                                            child: Text(
+                                              'x${p['quantity']}',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'monospace',
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
                       ],
